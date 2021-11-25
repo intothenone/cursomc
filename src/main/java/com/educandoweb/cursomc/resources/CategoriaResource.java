6a606e49bcbb5c606cb561d1bd55b8d10ee92c4e
César Augusto <c-castle@hotmail.com>
2021-11-25 15:36:18 -0300

    Criando repository e service para Categoria

--- src/main/java/com/educandoweb/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/educandoweb/cursomc/resources/CategoriaResource.java
@@ -1,27 +1,37 @@
 package com.educandoweb.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
+//import java.util.ArrayList;
+//import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.cursomc.domain.Categoria;
+import com.educandoweb.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
-
-		Categoria car1 = new Categoria(1, "Bricolagem");
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value = "/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(car1);
-		
-		return lista;
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+//
+//		Categoria car1 = new Categoria(1, "Bricolagem");
+//		
+//		List<Categoria> lista = new ArrayList<>();
+//		lista.add(car1);
+//		
+//		return lista;
 	}
 
 }
