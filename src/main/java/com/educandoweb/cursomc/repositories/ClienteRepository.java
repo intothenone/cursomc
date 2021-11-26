dc5f14188426b21c780d043eb3b211c22a23ad71
César Augusto <c-castle@hotmail.com>
2021-11-26 11:55:01 -0300

    Cliente, TipoCliente, Telefones e enderecos

---
+++ src/main/java/com/educandoweb/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.educandoweb.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.educandoweb.cursomc.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+
+}
