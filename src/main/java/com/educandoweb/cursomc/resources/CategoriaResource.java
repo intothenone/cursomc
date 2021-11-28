2e90ca3364dad6142f04a9a46c1fded1d7e40809
César Augusto <c-castle@hotmail.com>
2021-11-28 15:52:49 -0300

    Inserindo uma nova Categoria

--- src/main/java/com/educandoweb/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/educandoweb/cursomc/resources/CategoriaResource.java
@@ -1,14 +1,18 @@
 package com.educandoweb.cursomc.resources;
 
+import java.net.URI;
+
 //import java.util.ArrayList;
 //import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.cursomc.domain.Categoria;
 import com.educandoweb.cursomc.services.CategoriaService;
@@ -25,13 +29,13 @@ public class CategoriaResource {
 		
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
-//
-//		Categoria car1 = new Categoria(1, "Bricolagem");
-//		
-//		List<Categoria> lista = new ArrayList<>();
-//		lista.add(car1);
-//		
-//		return lista;
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
