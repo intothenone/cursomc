1b3de3dd263750c8570b26f4c9bfafb8fc78a0e6
César Augusto <c-castle@hotmail.com>
2021-12-08 10:03:18 -0300

    Subtotal do pedido

--- src/main/java/com/educandoweb/cursomc/domain/Pedido.java
+++ src/main/java/com/educandoweb/cursomc/domain/Pedido.java
@@ -58,6 +58,14 @@ public class Pedido implements Serializable{
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
 	
+	public double getValorTotal() {
+		double soma = 0;
+		for (ItemPedido ip : itens) {
+			soma = soma + ip.getSubTotal();
+		}
+		return soma;
+	}
+	
 	public List<Pedido> getPedidos(){
 		List<Pedido> lista = new ArrayList<>();
 		for (ItemPedido x : itens) {
