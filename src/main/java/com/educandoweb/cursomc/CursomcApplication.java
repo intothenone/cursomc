c6e2d7fab9b3a2c3f65d99b25474b3102398c531
César Augusto <c-castle@hotmail.com>
2021-12-29 17:22:11 -0300

    Enviando imagem via endpoint

--- src/main/java/com/educandoweb/cursomc/CursomcApplication.java
+++ src/main/java/com/educandoweb/cursomc/CursomcApplication.java
@@ -1,24 +1,17 @@
 package com.educandoweb.cursomc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.educandoweb.cursomc.services.S3Service;
-
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
-	
-	@Autowired
-	private S3Service s3service;
-	
+		
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
-	public void run(String... args) throws Exception {		
-		s3service.uploadFile("C:\\Users\\c-cas\\Downloads\\Lara.png");		
+	public void run(String... args) throws Exception {			
 	}	
 }
 
--- src/main/java/com/educandoweb/cursomc/resources/ClienteResource.java
+++ src/main/java/com/educandoweb/cursomc/resources/ClienteResource.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.cursomc.domain.Cliente;
@@ -84,4 +85,11 @@ public class ClienteResource {
 		return ResponseEntity.ok().body(listDTO);
 	}
 
+	@Transactional
+	@RequestMapping(value = "/picture", method=RequestMethod.POST)
+	public ResponseEntity<Void> uploadProfilePicture(@RequestParam(name="file") MultipartFile file) {
+		
+		URI uri = service.uploadProfilePicture(file);
+		return ResponseEntity.created(uri).build();
+	}
 }
